2bb76284b59714abbaaaadba19db9263a3bfd3ba
Gabriel Campari Correia <126830284+gabrielcampari@users.noreply.github.com>
2023-11-17 07:12:45 -0300

    Update crud.c

--- crud.c
+++ crud.c
@@ -1,1 +1,27 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct Data tipoData;
+
+typedef struct Atendimento tipoDisc; 
+
+struct Data {
+    int dia;
+    int mes;
+    int ano;
+};
+
+struct Atendimento {
+    int id_cliente;
+    int id_animal;
+    int id_servico;
+    float preço; 
+    char formadepagamento[50];
+};
+
+int main(void){
+
+    system("pause"); 
+    return 0;
+}
